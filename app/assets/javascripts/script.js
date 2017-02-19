caa97a8ffe9b5e4b7244c9ed86655f8617f38d97
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-19 16:05:03 +0000

    Improved output of location name

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,9 +6,15 @@ function gettingWeather(e){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
         var temperatureFarenheit = json.query.results.channel.item.condition.temp;
         var weatherConditions = json.query.results.channel.item.condition.text;
-        weatherOutput.innerHTML = "Temperature in "+weatherLocation+": "+temperatureFarenheit+"F "+weatherConditions;
+        weatherOutput.innerHTML = "Temperature in "+locationName(weatherLocation)+": "+temperatureFarenheit+"F "+weatherConditions;
     });
 };
+
+function locationName(name) {
+    return name.split("-").map(function(subString){
+        return subString[0].toUpperCase()+subString.substring(1);
+    }).join(" ");
+}
 
 var weatherButtons = document.querySelectorAll(".weatherButtons");
 weatherButtons.forEach(weatherButton => weatherButton.addEventListener('click', gettingWeather));
