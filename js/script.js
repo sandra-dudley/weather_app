3c47664eab60324bef8eb3bb411c4492bce51775
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-19 16:30:45 +0000

    Removed old redundant folder

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var weatherApiKey = config.weatherApiKey;
-function gettingWeather(){
-$.getJSON("//api.openweathermap.org/data/2.5/weather?q=London&APPID="+weatherApiKey,function(json){
-    document.write(json);
-});
-};
