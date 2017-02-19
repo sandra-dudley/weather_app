6e952f7ceb66cfcd1ded94b9b9a5b4a61b49c0b3
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-19 15:55:48 +0000

    Improved weather output

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,7 +4,9 @@ function gettingWeather(e){
     var weatherLocation = e.target.dataset.location;
     // https://developer.yahoo.com/weather/
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
-        weatherOutput.innerHTML = JSON.stringify(json);
+        var temperatureFarenheit = json.query.results.channel.item.condition.temp;
+        var weatherConditions = json.query.results.channel.item.condition.text;
+        weatherOutput.innerHTML = "Temperature in "+weatherLocation+": "+temperatureFarenheit+"F "+weatherConditions;
     });
 };
 
