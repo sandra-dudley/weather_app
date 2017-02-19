3a2ebd62203da268e6e91046a5e24db8187e1eb7
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-19 16:07:59 +0000

    Changed temperature from F to C

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,7 +6,7 @@ function gettingWeather(e){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
         var temperatureFarenheit = json.query.results.channel.item.condition.temp;
         var weatherConditions = json.query.results.channel.item.condition.text;
-        weatherOutput.innerHTML = "Temperature in "+locationName(weatherLocation)+": "+temperatureFarenheit+"F "+weatherConditions;
+        weatherOutput.innerHTML = "Temperature in "+locationName(weatherLocation)+": "+temperatureCelsius(temperatureFarenheit)+"C "+weatherConditions;
     });
 };
 
@@ -16,5 +16,8 @@ function locationName(name) {
     }).join(" ");
 }
 
+function temperatureCelsius (temp) {
+    return Math.round((temp-32)*(5/9)); 
+}
 var weatherButtons = document.querySelectorAll(".weatherButtons");
 weatherButtons.forEach(weatherButton => weatherButton.addEventListener('click', gettingWeather));
