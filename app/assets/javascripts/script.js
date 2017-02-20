d045e8fba1b76473a775238cbacf09ca85325653
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-20 13:11:43 +0000

    Separated city and temperature and added space for time, improved style

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,4 @@
+var cityOutput = document.getElementById("city_output");
 var weatherOutput = document.getElementById("weather_output");
 var body = document.body;
 function gettingWeather(e){
@@ -7,7 +8,8 @@ function gettingWeather(e){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
         var temperatureFarenheit = json.query.results.channel.item.condition.temp;
         var weatherConditions = json.query.results.channel.item.condition.text;
-        weatherOutput.innerHTML = "Temperature in "+locationName(weatherLocation)+": "+temperatureCelsius(temperatureFarenheit)+"C "+weatherConditions;
+        cityOutput.innerHTML = locationName(weatherLocation);
+        weatherOutput.innerHTML = temperatureCelsius(temperatureFarenheit)+"C <br/>"+weatherConditions;
         body.className= weatherLocation+"-bg";
     });
 };
