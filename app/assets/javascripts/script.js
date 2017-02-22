bbcb65260dbe0240ae300b2e3dd60d787afe8b8c
Sandra Dudley <sandra.dudley@ymail.com>
2017-02-22 08:16:34 +0000

    Added functions to avoid repetitions

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,6 @@
+"use strict";
 var body = document.body;
+/* weather */
 var cityOutput = document.getElementById("city-output");
 var weatherOutput = document.getElementById("weather-output");
 var weatherLocation;
@@ -6,27 +8,48 @@ var temperatureFar;
 var temperatureCel;
 var temperatureUnit = "C";
 var weatherConditions;
-var timeZone = "Europe/London";
-gettingWeather();
+var timeZone;
+
+/* time */
+var secondHand = document.querySelector('.second-hand');
+var minuteHand = document.querySelector('.min-hand');
+var hourHand = document.querySelector('.hour-hand');
+var now;
+
+function init() {
+    gettingWeather();
+}
+
 /* weather */
-function gettingWeather(e){
-    if (e === undefined) {
+function gettingWeather(){
+    if (!arguments[0]) {
         weatherLocation =  'london';
         timeZone = "Europe/London";
     } else {
+        var e = arguments[0];
         weatherLocation = e.target.dataset.location;
         timeZone = e.target.dataset.timezone;
     }
     // https://developer.yahoo.com/weather/
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
+        /* location */
+        cityOutput.innerHTML = locationName(weatherLocation);
+        body.className= weatherLocation+"-bg";
+        
+        /* temperature and weather condition */
+        
         temperatureFar = json.query.results.channel.item.condition.temp;
         temperatureCel = temperatureCelsius(temperatureFar);
         weatherConditions = json.query.results.channel.item.condition.text;
-        cityOutput.innerHTML = locationName(weatherLocation);
-        weatherOutput.innerHTML = "<span class='temperature'>"+temperatureCelsius(temperatureFar)+"°"+temperatureUnit+" </span><br/>"+weatherConditions;
-        body.className= weatherLocation+"-bg";
+        weatherOut(temperatureUnit);
+        
     });
 };
+
+function weatherOut(unit) {
+    weatherOutput.innerHTML = "<span class='temperature'>"+((unit==='C')?temperatureCel:temperatureFar)+"°"+temperatureUnit+" </span><br/>"+weatherConditions;
+}
+
 function locationName(name) {
     return name.split("-").map(function(subString){
         return subString[0].toUpperCase()+subString.substring(1);
@@ -36,56 +59,43 @@ function locationName(name) {
 function temperatureCelsius (temp) {
     return Math.round((temp-32)*(5/9)); 
 }
-function toggleUnit (e) {
-    if (temperatureUnit === "C") {
-        temperatureUnit = "F";
-        weatherOutput.innerHTML = "<span class='temperature'>"+temperatureFar+"°"+temperatureUnit+" </span><br/>"+weatherConditions;
-    } else {
-        temperatureUnit = "C";
-        weatherOutput.innerHTML = "<span class='temperature'>"+temperatureCel+"°"+temperatureUnit+" </span><br/>"+weatherConditions;
-    }
+function toggleUnit () {
+    temperatureUnit= (temperatureUnit === "C")?"F":"C";
+    weatherOut(temperatureUnit);
 }
 
 /* time */
-const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+
+
+function setHand(el, time, deg, trans) {
+    var degree = ((time / deg) * 360) + 90; // because of transform: rotate(90deg); in the CSS
+    if (degree < trans) { //  1sec = 6deg => 10 deg + 90deg //  1mn = 6deg => 10 deg + 90deg // 1hr = 30deg -> 40 deg + 90deg
+        el.style.transition = 'none';
+    } else {
+        el.style.transition = 'all 0.05s';
+    }
+    el.style.transform = 'rotate('+degree+'deg)';
+}
 
 function setDate() {
-
-    const now = moment().tz(timeZone);
-    const secondDate = now.seconds();
-    const secondDegree = ((secondDate / 60) * 360) + 90; // because of transform: rotate(90deg); in the CSS
-    if (secondDegree < 100) { //  1sec = 6deg => 10 deg + 90deg
-        secondHand.style.transition = 'none';
-    } else {
-        secondHand.style.transition = 'all 0.05s';
-    }
-    secondHand.style.transform = 'rotate('+secondDegree+'deg)';
+    now = moment().tz(timeZone);
     
-
-    const minuteDate = now.minutes();
-    const minuteDegree = ((minuteDate / 60) * 360) + 90;
-    if (minuteDegree < 100) { //  1mn = 6deg => 10 deg + 90deg
-        minuteHand.style.transition = 'none';
-    } else {
-        minuteHand.style.transition = 'all 0.05s';
-    }
-    minuteHand.style.transform = 'rotate('+minuteDegree+'deg)';
+    var secondDate = now.seconds();
+    setHand(secondHand,secondDate, 60, 100);
     
-    const hourDate = now.hours();
-    const hourDegree = ((hourDate / 12) * 360) + 90;
-    if (hourDegree < 130) { // 1hr = 30deg -> 40 deg + 90deg
-        hourHand.style.transition = 'none';
-    } else {
-        hourHand.style.transition = 'all 0.05s';
-    }
-    hourHand.style.transform = 'rotate('+hourDegree+'deg)';
+    var minuteDate = now.minutes();
+    setHand(minuteHand,minuteDate, 60, 100);
+    
+    var hourDate = now.hours();
+    setHand(hourHand,hourDate, 12, 130);
 }
+
 setInterval(setDate, 1000);
 
-
+/* event listeners */
 var weatherButtons = document.querySelectorAll(".weatherButtons");
 weatherButtons.forEach(weatherButton => weatherButton.addEventListener('click', gettingWeather));
 var toggleButton = document.querySelector(".toggleButton");
 toggleButton.addEventListener('click', toggleUnit);
+
+init();
