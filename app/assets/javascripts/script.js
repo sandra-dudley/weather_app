25eed8d8d83cc8795a4f56ab29e85ea01180c085
Sandra Dudley <sandra.dudley@ymail.com>
2017-03-01 13:41:34 +0000

    Added time of day

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -17,6 +17,8 @@ var secondHand = document.querySelector('.second-hand');
 var minuteHand = document.querySelector('.min-hand');
 var hourHand = document.querySelector('.hour-hand');
 var now;
+var timeOfDay;
+var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function init() {
     //select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text="(lat,long)")
@@ -46,8 +48,6 @@ function init() {
            gettingWeather();
         });
       }  
-      //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D%22(40.7141667%2C-74.0063889)%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
-    
 }
 
 /* weather */
@@ -63,10 +63,13 @@ function gettingWeather(){
         weatherLocation = e.target.dataset.location;
         timeZone = e.target.dataset.timezone;
     }
+    timeOfDay = getTimeOfDay(moment.tz(timeZone));
+    
     // https://developer.yahoo.com/weather/
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+weatherLocation+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",function(json){
         /* location */
-        cityOutput.innerHTML = locationName(weatherLocation);
+        
+        cityOutput.innerHTML = timeOfDay+" in "+locationName(weatherLocation);
         body.className= weatherLocation+"-bg";
         
         /* temperature and weather condition */
@@ -128,6 +131,23 @@ function setDate() {
 }
 
 setInterval(setDate, 1000);
+
+function getTimeOfDay(time) {
+    var partOfDay;
+    if (time.hours()>=0 && time.hours <=5) {
+        partOfDay = "early morning";
+    } else if (time.hours()>=6 && time.hours() <= 11) {
+        partOfDay = "morning";
+    } else if (time.hours()>=12 && time.hours() <= 16) {
+        partOfDay = "afternoon";
+    } else if (time.hours()>=17 && time.hours() <= 20) {
+        partOfDay = "evening";
+    } else {
+        partOfDay = "night";
+    }
+    
+    return weekDays[time.days()]+" "+partOfDay;
+}
 
 /* event listeners */
 var weatherButtons = document.querySelectorAll(".weatherButtons");
